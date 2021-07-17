e10a371668647fa0376db5826e88e2eaa7fd5b51
maxd66 <max.dunn63@gmail.com>
2021-07-17 02:14:21 -0700

    changed script file name, added font links, added question widget and button. Styled question.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// const setBackgroundImg = () => {
+//     $('html').attr('style', 'background-image: url(./assets/images/rain2.jpg); background-position: center; background-size: cover; height: 100%; width: 100%;')
+// }
+
+// setBackgroundImg();
