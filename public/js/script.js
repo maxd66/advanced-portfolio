c88bbd654308a3a181cda62a6e9ead507e1cdd46
maxd66 <max.dunn63@gmail.com>
2021-07-19 01:05:17 -0700

    added logic for cycling through the questions by question number

--- public/js/script.js
+++ public/js/script.js
@@ -32,6 +32,7 @@ class GenHtml {
 
 const genHtml = new GenHtml;
 
+// =========================== Next Class ===================================
 class QandA {
     constructor(question, buttons) {
       this.questionHtml = genHtml.genQuestionHtml(question);
@@ -47,6 +48,9 @@ class QandA {
     }
 }
 
+
+// ============================ Script specific for this index.html ===================================
+let questionNumber = 0;
 const mainQuestion = `My name is L.I.L.I and I was designed to help you find whatever you are looking for! Please select the option that best describes you! P.S. you can always switch to different pages with the buttons in the Nav Bar!`
 const mainButtons = ['I am a possible future employer.', 'I am a curious family member or friend.', 'I am a fellow developer looking for inspiration.', 'Where am I? How did I get here?']
 
@@ -55,4 +59,32 @@ const qAndA = new QandA(mainQuestion, mainButtons);
 document.getElementById('start-button').addEventListener('click', () => {
     qAndA.appendQuestion();
     qAndA.appendButtons();
+    questionNumber+= 1
+})
+
+// Save class with constructor to hold hasStarted and question number, both pulled from local storage
+// Use local storage as save functionality, first time they click game button set hasStarted equal to true
+// in local storage. If hasStarted is true when they click the game button, 
+// ask if they would like to go back to the question they were last on,
+// if has started equals false, set question number equal to 0. Would need to save each time they progress
+// by updating the questionNumber in local storage
+
+
+document.getElementById('all-buttons-container').addEventListener('click', (e) => {
+  switch(questionNumber) {
+    case 1:
+      if(e.target === document.getElementById('button0')) {
+        window.location = '/employer'
+      } else if (e.target === document.getElementById('button1')) {
+
+      } else if (e.target === document.getElementById('button2')) {
+
+      } else if (e.target === document.getElementById('button3')) {
+
+      }
+      break
+    case 2:
+      break
+  }
+  
 })
