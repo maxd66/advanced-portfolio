76932ae1fcf7bc758aafb26ac4f03579298788ac
maxd66 <max.dunn63@gmail.com>
2021-08-10 10:53:32 -0700

    Added meta descriptions to main pages and added starter files for hangman

---
+++ public/js/hangman.js
@@ -0,0 +1,77 @@
+// tasks:
+// Create array of tons of phrases, there should be so many that the user shouldn't see the same one after playing dozens of times
+// create funtion to generate word object for selected phrase
+// create function to append words to page, called every time an input is registered
+// create function to check user input and replace letters when necessary
+const wordArr = [];
+let chosenWord = "";
+let lettersInChosen;
+let displayLetters = [];
+let guessedLetters = [];
+let lives = 7;
+
+const setWord = () => {
+  chosenWord =
+    wordArr[Math.floor(Math.random() * wordArr.length)].toUpperCase();
+  lettersInChosen = chosenWord.split("");
+  for (let i = 0; i < lettersInChosen.length; i++) {
+    if (
+      lettersInChosen[i] === " " ||
+      lettersInChosen[i] === "-" ||
+      lettersInChosen[i] === ","
+    ) {
+      displayLetters.push(lettersInChosen[i]);
+    } else {
+      displayLetters.push("_");
+    }
+  }
+};
+
+const renderWord = (letterArr) => {
+  const wordString = letterArr.join(" ").replace(" ", "&nbsp;");
+  document.getElementById("wordBlanks").textContent = wordString;
+};
+
+const checkLetter = (letter) => {
+  let needRender = false;
+  guessedLetters.push(letter);
+  for (let i = 0; i < lettersInChosen.length; i++) {
+    if (letter.toLowerCase() === lettersInChosen[i]) {
+      displayLetters[i] = letter;
+      needRender = true;
+    }
+  }
+  if (needRender) {
+    renderWord(displayLetters);
+  } else {
+    lives--;
+    if (lives === 0) {
+      gameOver();
+    } else {
+      wrongLetter();
+    }
+  }
+};
+
+const wrongLetter = () => {};
+
+const gameOver = () => {};
+
+const handleSubmit = () => {
+  const userInput = document.getElementById("userInput").value.toUpperCase();
+  if (!userInput) {
+    return;
+  } else if (!userInput.match(/[A-Z]/)) {
+    alert(
+      "Traveler, have you not played this game before? Only guess letters!"
+    );
+    return;
+  } else {
+    checkLetter(userInput);
+  }
+};
+
+document.getElementById("hangmanForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  handleSubmit();
+});
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-const sequelize = require('./config/connection');
-const express = require('express');
-const Visitor = require('./models/Visitor');
-const { RSA_NO_PADDING } = require('constants');
+const fs = require("fs");
+const path = require("path");
+const sequelize = require("./config/connection");
+const express = require("express");
+const Visitor = require("./models/Visitor");
+const { RSA_NO_PADDING } = require("constants");
 
 const app = express();
 
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // The code below points the server to a series of "route" files. These routes
 // give our server a "map" of how to respond when users visit or request data
@@ -19,40 +19,43 @@ app.use(express.static('public'))
 // require('./routes/apiRoutes')(app);
 // require('./routes/htmlRoutes')(app);
 
-
 // Index get routes for HTML, CSS, and JavaScript
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 // GET requests
-app.get('/employer', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/employer.html'))
-})
-app.get('/family', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/family.html'))
-})
-app.get('/fellowdev', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/fellowdev.html'))
-})
-app.get('/api/visitors', async (req, res) => {
-    try {
-        const visData = await Visitor.findAll()
-        res.json(visData);
-    } catch(err) {
-        res.status(500).json(err);
-    }
-})
+app.get("/employer", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/employer.html"));
+});
+app.get("/family", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/family.html"));
+});
+app.get("/fellowdev", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/fellowdev.html"));
+});
+app.get("/api/visitors", async (req, res) => {
+  try {
+    const visData = await Visitor.findAll();
+    res.json(visData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+app.get("/hangman", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/hangman.html"));
+});
 
 // POST requests
-app.post('/api/visitors', async (req, res) => {
-    try {
-        const userData = await Visitor.create(req.body);
-        res.status(200).json(userData);
-    } catch(err) {res.status(400).json(err)}
-})
-
+app.post("/api/visitors", async (req, res) => {
+  try {
+    const userData = await Visitor.create(req.body);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
