a5b9312ccccf62a7fc2d63ff107d10a1c5410847
maxd66 <max.dunn63@gmail.com>
2021-08-12 02:30:58 -0700

    added style and logic to handle game over if lost, event listener for buttons added but no logic yet

--- public/js/hangman.js
+++ public/js/hangman.js
@@ -3,6 +3,61 @@
 // create funtion to generate word object for selected phrase
 // create function to append words to page, called every time an input is registered
 // create function to check user input and replace letters when necessary
+const gradualAppend = (question, btnArr) => {
+  const questionArr = question.split("");
+  let input = ``;
+  let appendIndex = 0;
+  let end = questionArr.length;
+  intervalFunc(questionArr, input, appendIndex, end, btnArr);
+};
+
+const intervalFunc = (questionArr, input, appendIndex, end, btnArr) => {
+  const myInterval = setInterval(() => {
+    input += questionArr[appendIndex];
+    const questionHTML = `<h2 class="question">${input}</h2>`;
+    document.getElementById("hangmanForm").innerHTML = questionHTML;
+    if (
+      questionArr[appendIndex] === "." ||
+      questionArr[appendIndex] === "!" ||
+      questionArr[appendIndex] === "?"
+    ) {
+      clearInterval(myInterval);
+      setTimeout(() => {
+        if (appendIndex >= end) {
+          clearInterval(myInterval);
+          putButtons(btnArr);
+          return;
+        } else {
+          intervalFunc(questionArr, input, appendIndex, end, btnArr);
+        }
+      }, 400);
+    }
+    appendIndex++;
+    if (appendIndex >= end) {
+      clearInterval(myInterval);
+      putButtons(btnArr);
+      return;
+    }
+  }, 100);
+};
+
+const clearButtons = () => {
+  document.getElementById("button-element-container").innerHTML = "";
+};
+
+const putButtons = (buttonArr) => {
+  let btnHtml = ``;
+  for (let i = 0; i < buttonArr.length; i++) {
+    const tempBtnHtml = ` <div class="button-element-container">
+    <section id="button-box${i + 1}" class="text-center mx-xl-2 p-3">
+    <button id="button${i}" class="p-4">${buttonArr[i]}</button>
+    </section>
+    </div>`;
+    btnHtml += tempBtnHtml;
+  }
+  document.getElementById("button-element-container").innerHTML = btnHtml;
+};
+
 const wordArr = ["In the direction of Deer"];
 let chosenWord;
 let lettersInChosen;
@@ -84,6 +139,18 @@ const wrongLetter = () => {
 
 const gameOver = () => {
   wrongLetter();
+  const message = `Oh no! You died...The answer was, ${chosenWord}, but good try. Do you want to play again?`;
+  const btnArr = [`Sure`, `No, lets try something else.`];
+  gradualAppend(message, btnArr);
+  document
+    .getElementById("button-element-container")
+    .addEventListener("click", (e) => {
+      if (e.target === document.getElementById("button0")) {
+        alert("this does something");
+      } else if (e.target === document.getElementById("button1")) {
+        alert("this does something else");
+      }
+    });
 };
 
 const handleSubmit = () => {
