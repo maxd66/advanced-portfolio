4c824dd2834ce0d3c3ad2cb208319ecb37a81d06
maxd66 <max.dunn63@gmail.com>
2021-08-14 23:52:21 -0700

    added file for prodigium logic and started planning

---
+++ public/js/prodigiumScript.js
@@ -0,0 +1,33 @@
+// need to have functions that will subtract hitpoints from players life based on attack type, crit chance and enemy type and defense
+// player attack objects could look like
+// player object could look like
+// const p1 = {
+//   name: "Gristle",
+//   type: "plant",
+//   hp: 150,
+//   adv: ["water", "mythical"],
+//   weak: ["fire", "ice"],
+//   moves: [ivySpikes, strangleRoot, regrow],
+// };
+// const ivySpikes = {
+//     damage: 20,
+//     critChance: 5,
+// };
+// const regrow = {
+//     heal: 10,
+//     critChance: 10,
+// }
+
+// enemy player object
+// const p2 = {
+//   name: "Koogle",
+//   type: "mythical",
+//   hp: 200,
+//   adv: ["fighting", "flying"],
+//   weak: ["plant", "lightning"],
+//   moves: [shadowSlice, minorIllusion, poisonMist],
+// };
+
+// function should take players in question, and move chosen
+// function will determine if move is heal, damage, or protect
+// then will calulate changes to players
