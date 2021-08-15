f123773b04477f542aeaa6dacd0fcc6634a1d1ac
maxd66 <max.dunn63@gmail.com>
2021-08-15 00:31:44 -0700

    completed logic for determine advantage

--- public/js/prodigiumScript.js
+++ public/js/prodigiumScript.js
@@ -1,30 +1,34 @@
 // need to have functions that will subtract hitpoints from players life based on attack type, crit chance and enemy type and defense
 // player attack objects could look like
 // player object could look like
-const p1 = {
-  name: "Gristle",
-  startHp: 200,
-  hp: 150,
-  moves: [ivySpikes, strangleRoot, regrow],
-};
+// const p1 = {
+//   name: "Gristle",
+//   type: "plant",
+//   startHp: 200,
+//   hp: 150,
+//   moves: [ivySpikes, strangleRoot, regrow],
+// };
 // const ivySpikes = {
-//     ahp: "attack",
-//     damage: 20,
-//     critChance: 5,
-//     type: "plant",
-//     adv: ["water", "mythical"],
-//     weak: ["fire", "ice"],
+//   ahp: "attack",
+//   damage: 20,
+//   critChance: 5,
+//   type: "plant",
+//   adv: ["water", "mythical"],
+//   weak: ["fire", "ice"],
+//   onCool: false,
 // };
 // const regrow = {
 //   ahp: "heal",
 //   heal: 10,
 //   critChance: 10,
 //   type: "neutral",
+//   onCool: false,
 // };
 
 // enemy player object
 // const p2 = {
 //   name: "Koogle",
+//   type: "mythical",
 //   startHp: 220,
 //   hp: 200,
 //   moves: [shadowSlice, minorIllusion, poisonMist],
@@ -36,6 +40,15 @@ const p1 = {
 
 const determineAdv = (move, p2) => {
   const type = move.type;
+  if (type === "neutral") {
+    return 0;
+  } else if (move.adv.includes(p2.type)) {
+    return 1;
+  } else if (move.weak.includes(p2.type)) {
+    return -1;
+  } else {
+    return 0;
+  }
 };
 
 const calculateEffect = (p1, p2, move) => {
