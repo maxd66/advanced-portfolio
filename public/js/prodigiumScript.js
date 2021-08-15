26b1148a18df7a8657686d2719379a059a339210
maxd66 <max.dunn63@gmail.com>
2021-08-15 00:31:22 -0700

    adding logic for calculating effects and advantage

--- public/js/prodigiumScript.js
+++ public/js/prodigiumScript.js
@@ -1,33 +1,57 @@
 // need to have functions that will subtract hitpoints from players life based on attack type, crit chance and enemy type and defense
 // player attack objects could look like
 // player object could look like
-// const p1 = {
-//   name: "Gristle",
-//   type: "plant",
-//   hp: 150,
-//   adv: ["water", "mythical"],
-//   weak: ["fire", "ice"],
-//   moves: [ivySpikes, strangleRoot, regrow],
-// };
+const p1 = {
+  name: "Gristle",
+  startHp: 200,
+  hp: 150,
+  moves: [ivySpikes, strangleRoot, regrow],
+};
 // const ivySpikes = {
+//     ahp: "attack",
 //     damage: 20,
 //     critChance: 5,
+//     type: "plant",
+//     adv: ["water", "mythical"],
+//     weak: ["fire", "ice"],
 // };
 // const regrow = {
-//     heal: 10,
-//     critChance: 10,
-// }
+//   ahp: "heal",
+//   heal: 10,
+//   critChance: 10,
+//   type: "neutral",
+// };
 
 // enemy player object
 // const p2 = {
 //   name: "Koogle",
-//   type: "mythical",
+//   startHp: 220,
 //   hp: 200,
-//   adv: ["fighting", "flying"],
-//   weak: ["plant", "lightning"],
 //   moves: [shadowSlice, minorIllusion, poisonMist],
 // };
 
 // function should take players in question, and move chosen
 // function will determine if move is heal, damage, or protect
 // then will calulate changes to players
+
+const determineAdv = (move, p2) => {
+  const type = move.type;
+};
+
+const calculateEffect = (p1, p2, move) => {
+  const crit = Math.floor(Math.random() * 100);
+  const adv = determineAdv(move, p2);
+  switch (move.ahp) {
+    case "attack":
+      if (crit < move.critChance) {
+        p2.hp -= move.damage * 3;
+      } else {
+        p2.hp -= move.damage;
+      }
+      break;
+    case "heal":
+      break;
+    case "protect":
+      break;
+  }
+};
